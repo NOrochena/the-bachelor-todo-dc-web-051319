7b38fe652b814bb7e44edb724abaa46d80d31cc8
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-04 05:10:13 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_number, value|
+    if season_number == season
+      value.each do |key, value|
+        if key["status"] == "Winner"
+          arr = key["name"].split(" ")
+          return arr[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_number, value|
+    value.each do |key, attribute|
+      if key["occupation"] == occupation
+        return key["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season_number, value|
+    value.each do |key, attribute|
+      if key["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, value|
+    value.each do |key, attribute|
+      if key["hometown"] == hometown
+        return key["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  arr = []
+  num = 0
+  
+  data.each do |season_number, value|
+    if season_number == season
+      value.each do |key, attribute|
+        arr.push(key["age"].to_i)
+      end
+    end
+  end
+  arr.each do |age|
+    num += age
+  end
+  (num.to_f / arr.length.to_f).round
 end
